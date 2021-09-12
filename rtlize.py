134152b963e3618ca573bac2c5497c9d670770ca
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-09-12 10:51:07 +0430

    refactor Rtlize.py

--- rtlize.py
+++ rtlize.py
@@ -6,31 +6,30 @@ from os.path import join as join_paths
 B_RTL = """<div dir="rtl">"""
 E_RTL = """</div>"""
 
+
 def check_rtl(checking_file):
-    handler = open(checking_file,"r")
+    handler = open(checking_file, mode="r", encoding="utf-8")
     file_content = handler.read()
     handler.close()
     if file_content.strip().startswith(B_RTL):
         print(f"{checking_file} is ok!")
-        return 
-    else:
-        handler = open(checking_file,"w")
-        modified_content =f"{B_RTL}\n\n{file_content}\n{E_RTL}\n"
-        handler.write(modified_content)
-        handler.close()
-        print(f"mission completed!{checking_file} modified successfully!!!!=)")
+        return
 
-for dirpath, _ , fnames in walk("./"):
-    for f in fnames:
-        if f.endswith(".md"):
-            file_path =(join_paths(dirpath, f))
-            check_rtl(file_path)
+    handler = open(checking_file, mode="w", encoding="utf-8")
+    modified_content = f"{B_RTL}\n\n{file_content}\n{E_RTL}\n"
+    handler.write(modified_content)
+    handler.close()
+    print(f"mission completed!{checking_file} modified successfully!!!!=)")
 
 
-    
-    
- 
-    
+def main():
+    for dirpath, _, file_names in walk("./"):
+        for file_name in file_names:
+            if file_name.endswith(".md"):
+                file_path = (join_paths(dirpath, file_name))
+                check_rtl(file_path)
 
-    
-            
+
+if __name__ == "__main__":
+    main()
+
